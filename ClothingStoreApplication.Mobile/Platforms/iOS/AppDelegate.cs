f6dfff6dfa36fe2d851162a733a2a070c95245f8
TimoceDarius <115105281+TimoceDarius@users.noreply.github.com>
2024-08-20 21:42:32 +0300

    initial commit

---
+++ ClothingStoreApplication.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ClothingStoreApplication.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

8c8b401466c59815fc465e2d4ac091f4f4b484c3
Darius Timoce <74290441+MadeOfDye@users.noreply.github.com>
2024-08-26 10:40:38 +0300

    Further Entities modified.

--- ClothingStoreApplication.Api/Core/Domain/Overshirt.cs
+++ ClothingStoreApplication.Api/Core/Domain/Overshirt.cs
@@ -1,6 +1,160 @@
 ﻿namespace ClothingStoreApplication.Api.Core.Domain
 {
-    public class Overshirt
+    public class Overshirt : ClothingItem
     {
+        public string FitType { get; private set; }
+        public string ClosureType { get; private set; }
+        public string PocketConfiguration { get; private set; }
+        public string Lining { get; private set; }
+        public string CuffStyle { get; private set; }
+        public string HemStyle { get; private set; }
+        public string CollarStyle { get; private set; }
+
+        private Overshirt() { }
+
+        public class Builder
+        {
+            private readonly Overshirt _overshirt = new Overshirt();
+
+            public Builder SetId(Guid id)
+            {
+                _overshirt.Id = id;
+                return this;
+            }
+
+            public Builder SetName(string name)
+            {
+                _overshirt.Name = name;
+                return this;
+            }
+
+            public Builder SetDescription(string description)
+            {
+                _overshirt.Description = description;
+                return this;
+            }
+
+            public Builder SetColor(string color)
+            {
+                _overshirt.Color = color;
+                return this;
+            }
+
+            public Builder SetMaterial(string material)
+            {
+                _overshirt.Material = material;
+                return this;
+            }
+
+            public Builder SetPrice(decimal price)
+            {
+                _overshirt.Price = price;
+                return this;
+            }
+
+            public Builder SetIsDiscount(bool isDiscount)
+            {
+                _overshirt.IsDiscount = isDiscount;
+                return this;
+            }
+
+            public Builder SetDiscountAmount(double discountAmount)
+            {
+                _overshirt.DiscountAmount = discountAmount;
+                return this;
+            }
+
+            public Builder SetMaterialComposition(string composition)
+            {
+                _overshirt.MaterialComposition = composition;
+                return this;
+            }
+
+            public Builder SetSize(string size)
+            {
+                _overshirt.Size = size;
+                return this;
+            }
+
+            public Builder SetNewArrival(bool newArrival)
+            {
+                _overshirt.IsNewArrival = newArrival;
+                return this;
+            }
+
+            public Builder SetIsBestSeller(bool isBestSeller)
+            {
+                _overshirt.IsBestSeller = isBestSeller;
+                return this;
+            }
+
+            public Builder SetCollection(string collection)
+            {
+                _overshirt.Collection = collection;
+                return this;
+            }
+
+            public Builder SetStyle(string style)
+            {
+                _overshirt.Style = style;
+                return this;
+            }
+
+            public Builder SetGender(string gender)
+            {
+                _overshirt.Gender = gender;
+                return this;
+            }
+
+            public Builder SetFitType(string fitType)
+            {
+                _overshirt.FitType = fitType;
+                return this;
+            }
+
+            public Builder SetClosureType(string closureType)
+            {
+                _overshirt.ClosureType = closureType;
+                return this;
+            }
+
+            public Builder SetPocketConfiguration(string pocketConfiguration)
+            {
+                _overshirt.PocketConfiguration = pocketConfiguration;
+                return this;
+            }
+
+            public Builder SetLining(string lining)
+            {
+                _overshirt.Lining = lining;
+                return this;
+            }
+
+            public Builder SetCuffStyle(string cuffStyle)
+            {
+                _overshirt.CuffStyle = cuffStyle;
+                return this;
+            }
+
+            public Builder SetHemStyle(string hemStyle)
+            {
+                _overshirt.HemStyle = hemStyle;
+                return this;
+            }
+
+            public Builder SetCollarStyle(string collarStyle)
+            {
+                _overshirt.CollarStyle = collarStyle;
+                return this;
+            }
+
+            // Build method to return the final Overshirt object
+            public Overshirt Build()
+            {
+                if (string.IsNullOrWhiteSpace(_overshirt.Name)) throw new InvalidOperationException("Name is required");
+
+                return _overshirt;
+            }
+        }
     }
 }
--- ClothingStoreApplication.Api/Core/Domain/RunningShoes.cs
+++ ClothingStoreApplication.Api/Core/Domain/RunningShoes.cs
@@ -1,6 +1,174 @@
 ﻿namespace ClothingStoreApplication.Api.Core.Domain
 {
-    public class RunningShoes
+    public class RunningShoes : ClothingItem
     {
+        public string ShoeWeight { get; private set; }
+        public string DropHeight { get; private set; }
+        public string CushioningLevel { get; private set; }
+        public string UpperMaterial { get; private set; }
+        public string Breathability { get; private set; }
+        public string TreadType { get; private set; }
+        public string StabilityFeatures { get; private set; }
+        public string HeelCounter { get; private set; }
+        public string LacingSystem { get; private set; }
+
+        private RunningShoes() { }
+
+        public class Builder
+        {
+            private readonly RunningShoes _runningShoes = new RunningShoes();
+
+            public Builder SetId(Guid id)
+            {
+                _runningShoes.Id = id;
+                return this;
+            }
+
+            public Builder SetName(string name)
+            {
+                _runningShoes.Name = name;
+                return this;
+            }
+
+            public Builder SetDescription(string description)
+            {
+                _runningShoes.Description = description;
+                return this;
+            }
+
+            public Builder SetColor(string color)
+            {
+                _runningShoes.Color = color;
+                return this;
+            }
+
+            public Builder SetMaterial(string material)
+            {
+                _runningShoes.Material = material;
+                return this;
+            }
+
+            public Builder SetPrice(decimal price)
+            {
+                _runningShoes.Price = price;
+                return this;
+            }
+
+            public Builder SetIsDiscount(bool isDiscount)
+            {
+                _runningShoes.IsDiscount = isDiscount;
+                return this;
+            }
+
+            public Builder SetDiscountAmount(double discountAmount)
+            {
+                _runningShoes.DiscountAmount = discountAmount;
+                return this;
+            }
+
+            public Builder SetMaterialComposition(string composition)
+            {
+                _runningShoes.MaterialComposition = composition;
+                return this;
+            }
+
+            public Builder SetSize(string size)
+            {
+                _runningShoes.Size = size;
+                return this;
+            }
+
+            public Builder SetNewArrival(bool newArrival)
+            {
+                _runningShoes.IsNewArrival = newArrival;
+                return this;
+            }
+
+            public Builder SetIsBestSeller(bool isBestSeller)
+            {
+                _runningShoes.IsBestSeller = isBestSeller;
+                return this;
+            }
+
+            public Builder SetCollection(string collection)
+            {
+                _runningShoes.Collection = collection;
+                return this;
+            }
+
+            public Builder SetStyle(string style)
+            {
+                _runningShoes.Style = style;
+                return this;
+            }
+
+            public Builder SetGender(string gender)
+            {
+                _runningShoes.Gender = gender;
+                return this;
+            }
+
+            public Builder SetShoeWeight(string shoeWeight)
+            {
+                _runningShoes.ShoeWeight = shoeWeight;
+                return this;
+            }
+
+            public Builder SetDropHeight(string dropHeight)
+            {
+                _runningShoes.DropHeight = dropHeight;
+                return this;
+            }
+
+            public Builder SetCushioningLevel(string cushioningLevel)
+            {
+                _runningShoes.CushioningLevel = cushioningLevel;
+                return this;
+            }
+
+            public Builder SetUpperMaterial(string upperMaterial)
+            {
+                _runningShoes.UpperMaterial = upperMaterial;
+                return this;
+            }
+
+            public Builder SetBreathability(string breathability)
+            {
+                _runningShoes.Breathability = breathability;
+                return this;
+            }
+
+            public Builder SetTreadType(string treadType)
+            {
+                _runningShoes.TreadType = treadType;
+                return this;
+            }
+
+            public Builder SetStabilityFeatures(string stabilityFeatures)
+            {
+                _runningShoes.StabilityFeatures = stabilityFeatures;
+                return this;
+            }
+
+            public Builder SetHeelCounter(string heelCounter)
+            {
+                _runningShoes.HeelCounter = heelCounter;
+                return this;
+            }
+
+            public Builder SetLacingSystem(string lacingSystem)
+            {
+                _runningShoes.LacingSystem = lacingSystem;
+                return this;
+            }
+
+            // Build method to return the final RunningShoes object
+            public RunningShoes Build()
+            {
+                if (string.IsNullOrWhiteSpace(_runningShoes.Name)) throw new InvalidOperationException("Name is required");
+
+                return _runningShoes;
+            }
+        }
     }
 }
